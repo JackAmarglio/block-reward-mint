2ec9bfa66f7cf49a34e43d2770a20539afdb2f59
JackAmarglio <topdevking@gmail.com>
2022-09-27 10:57:38 -0400

    last result

---
+++ config/local-example.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  collection: {
+    name: 'Beepboy Plant #',
+    unitName: 'BP',
+    image_mimetype: 'image/png',
+  },
+  wallet: {
+    addr: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', // Mint Wallet address
+    // Mint Wallet mnemonic words
+    mnemonic:
+      'word word word word word word word word word word word word word word word word word word word word word word word word word',
+  },
+  pinata: {
+    apiKey: 'xxxxxxxxxxxxxxxxxxxx', // pinata.cloud api key
+    apiSecret:
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // pinata.cloud api secret
+  },
+  algorand: {
+    token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    server: 'https://mainnet-algorand.api.purestake.io/ps2',
+    port: '',
+  },
+};
---
+++ src/lib/algorand/index.ts
@@ -0,0 +1,42 @@
+import algosdk from 'algosdk';
+import config from 'config';
+
+// Uncomment this if using Purestake
+/*
+const token = {
+  'x-api-key': config.get('algorand.token') as string,
+};
+*/
+//Comment next line if using Purestake
+const token = config.get('algorand.token') as string;
+
+const port = config.get('algorand.port') as string;
+const server = config.get('algorand.server') as string;
+export const algodClient: algosdk.Algodv2 = new algosdk.Algodv2(
+  token,
+  server,
+  port
+);
+
+export const getAccount = async () => {
+  try {
+    const myaccount = algosdk.mnemonicToSecretKey(
+      config.get('wallet.mnemonic') || ''
+    );
+    return myaccount;
+  } catch (err) {
+    console.log('err', err);
+  }
+
+  return null;
+};
+
+export const getAccountInfo = async (address: string) => {
+  const clientInfo = await algodClient.accountInformation(address).do();
+  return clientInfo;
+};
+
+export const getAsset = async (assetID: number) => {
+  const asset = await algodClient.getAssetByID(assetID).do();
+  return asset;
+};
